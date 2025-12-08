1c9900e93b6813bcde89bf56a974d2a36cf9163d
Avishek <149515418+xer0Xavishek@users.noreply.github.com>
2025-12-08 23:45:28 +0600

    Add create user table SQL script

--- src/sql/create_tables.sql
+++ src/sql/create_tables.sql
@@ -8,3 +8,25 @@
 
 USE skillswap;
 
+------------------------------------------------------
+-- 1. USER (SuperClass)
+------------------------------------------------------
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    
+    -- composite attribute split
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+
+    email VARCHAR(120) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role ENUM('admin','user') DEFAULT 'user',
+
+    preferred_language VARCHAR(50),
+
+    -- multivalued -> separate table (BCNF requirement)
+    city VARCHAR(80),
+    country VARCHAR(80),
+
+    skillpoints INT DEFAULT 0
+);
